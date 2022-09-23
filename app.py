6faa12470cdd7ef38f3cce4feb9c21b53347233a
YuvanLohia <lohiayuvan@gmail.com>
2022-09-23 19:37:04 +0530

    First commit

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask,render_template
-
+from flask import Flask,render_template,url_for
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+class Books(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    bookname = db.Column(db.String(100),nullable = False)
+    shelf = db.Column(db.Integer,primary_key=True)
+
 
 @app.route("/")
 def index():
